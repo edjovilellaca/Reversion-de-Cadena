cfe09ac2c7cfc098f4d28b52a7fc91eb7394b2b9
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-08-28 19:45:15 -0700

    Ya quedó

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log(cadena.split('').reverse().join('')); //Para invertir todos los carácteres.
+
+console.log(cadena.split(' ').reverse().join(' ')); //Para invertir el orden de las palabras.
